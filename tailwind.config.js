f8da65526415f21499fa1902a6c0d70b830dd3a9
mahinhossin81 <mahinhossin81@mail.com>
2024-07-06 16:21:28 +0600

    bg-image set

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      backgroundImage: {
+        'testimonial': ['url(/images/brooke-cagle-g1Kr4Ozfoac-unsplash\ 2.png)']
+      },
       boxShadow: {
         '3xl': 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
       },
